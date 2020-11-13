ae8897de38f908c24fb2825632ead3d620973486
Evadne Meraki <valemescudero@gmail.com>
2020-11-13 19:47:57 -0300

    Fixed button issues

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import './App.css';
 import Button from './components/Button';
 
 class App extends Component {
-render() {
-  return (
-    <div className="app">
-      <div className="calc-wrapper">
-        <div className="row">
-          <Button>7</Button>
+  render() {
+    return (<div className="app">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>x</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
         </div>
       </div>
-    </div>
-    );
-  }
+      );
+    }  
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
 
 const isOperator = val => {
-    return !isNaN(val) || val === '.' || val === '='
-}
+    return !isNaN(val) || val === '.' | val === '=';
+};
 
-const Button = props => {
-    <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
-        {props.children}
-    </div>
-}
+const Button = props => (<div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>{props.children}</div>);
 
 export default Button;
