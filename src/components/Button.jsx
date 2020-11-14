c845c7b014a3ab47700ed247ab4d997aae590381
Evadne Meraki <valemescudero@gmail.com>
2020-11-14 07:11:43 -0300

    New! Input and Buttons functionality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,4 @@ const isOperator = val => {
     return !isNaN(val) || val === '.' | val === '=';
 };
 
-const Button = props => (<div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>{props.children}</div>);
-
-export default Button;
+export const Button = props => (<div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`} onClick={() => props.handleClick(props.children)}>{props.children}</div>);
